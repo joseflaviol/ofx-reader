489c1a2926f737d19491b5058537b7fa5532126d
José Flávio <jose.flavio@adaptweb.com.br>
2024-11-01 09:08:41 -0300

    adding start and end date

--- src/OFX.php
+++ src/OFX.php
@@ -2,15 +2,39 @@
 
 namespace OFXReader;
 
+use DateTimeInterface;
 use OFXReader\Entity\Transaction;
 
 class OFX
 {
+    private DateTimeInterface $startDate;
+    private DateTimeInterface $endDate;
     private array $transactions;
 
     public function __construct()
     {
         $this->transactions = [];
+    }
+
+    public function setStartDate(DateTimeInterface $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function startDate(): DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+
+    public function setEndDate(DateTimeInterface $endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+    public function endDate(): DateTimeInterface
+    {
+        return $this->endDate;
     }
 
     public function addTransaction(Transaction $transaction)
